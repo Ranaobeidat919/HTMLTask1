31d13cbd23682a6b7cc39eb77e1cc1693b06db3e
Ranaobeidat919 <ranaobeidat505@gmail.com>
2024-07-21 21:16:49 +0300

    Resolved merge conflicts

---
+++ taskjs.js
@@ -0,0 +1,20 @@
+//string
+//int
+//bool
+//float
+//object
+//array
+
+var name ="rana";
+var age = 23;
+
+let name ="sara";
+let age=19.5;
+let name = "esraa";
+
+const name="esraa";
+const age=22;
+
+const arr = [1,"esraa",3];
+
+console.log(arr[1]);
